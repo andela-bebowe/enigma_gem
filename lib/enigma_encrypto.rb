19e4d2dd13cb10a349e26bb78a2c9db5111c82bc
Ebowe Blessing <blessing.ebowe@andela.com>
2015-10-05 11:44:39 +0100

    Code refactoring

--- lib/enigma_encrypto.rb
+++ lib/enigma_encrypto.rb
@@ -1,7 +1,4 @@
-require "enigma_encrypto/version"
-require "enigma_encrypto/crack"
-require "enigma_encrypto/encrypt"
-require "enigma_encrypto/decrypt"
+require "main_lib_helper.rb"
 
 module EnigmaEncrypto
   def self.encrypt
---
+++ lib/main_lib_helper.rb
@@ -0,0 +1,4 @@
+require "enigma_encrypto/version"
+require "enigma_encrypto/crack"
+require "enigma_encrypto/encrypt"
+require "enigma_encrypto/decrypt"
