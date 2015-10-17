a49e21057f437afa7778f802664c5fe7cad0785d
Ebowe Blessing <blessing.ebowe@andela.com>
2015-10-17 09:12:38 +0000

    writing better tests

--- lib/enigma_encrypto/file_handler.rb
+++ lib/enigma_encrypto/file_handler.rb
@@ -1,43 +1,43 @@
 module EnigmaEncrypto
-  class FileHandler
-    def invalid_file_message
-      "File to be read doesnt exist or is empty.\nTry again with an existing file"
-    end
+	class FileHandler
+		def invalid_file_message
+			"File to be read doesnt exist or is empty.\nTry again with an existing file"
+		end
 
-    def file_exist_message
-      "File to be written to already exists!\n" + 
-      "To cancel operation, Press 'C'\nTo overwrite File, Type 'W'"
-    end
+		def file_exist_message
+			"File to be written to already exists!\n" +
+			"To cancel operation, Press 'C'\nTo overwrite File, Type 'W'"
+		end
 
-    def check_file_useability(file1, file2)
-      if (!(File.exists?file1) || (File.zero? file1))
-        puts invalid_file_message
-        return false
-      end 
-      if File.exists?file2
-        puts file_exist_message
-        operation_collector
-      end
-    end
+		def check_file_useability(file1, file2)
+			if (!(File.exists?file1) || (File.zero? file1))
+				puts invalid_file_message
+				return false
+			end
+			if File.exists?file2
+				puts file_exist_message
+				operation_collector
+			end
+		end
 
-    def read(filename)
-      readable = File.open(filename, "r")
-    end
+		def read(filename)
+			readable = File.open(filename, "r")
+		end
 
-    def writer(filename, word)
-      writable = File.open(filename, "a")
-      writable.write(word)
-      writable.close
-    end
+		def writer(filename, word)
+			writable = File.open(filename, "a")
+			writable.write(word)
+			writable.close
+		end
 
-    private
-      def operation_collector
-        operation = ($stdin.gets.chomp).downcase
-        if operation != "w" && operation != "c"
-          puts "Incorrect Entry"
-          operation = false
-        end
-        return operation
-      end 
-  end
+		private
+			def operation_collector
+				operation = ($stdin.gets.chomp).downcase
+				if operation != "w" && operation != "c"
+					puts "Incorrect Entry"
+					operation = false
+				end
+				return operation
+			end
+	end
 end
--- spec/file_handler_spec.rb
+++ spec/file_handler_spec.rb
@@ -1,33 +1,50 @@
 require_relative "spec_helper"
 
 describe "FileHandler" do
-  before :each do
-    @filehandler = EnigmaEncrypto::FileHandler.new
-    @file1, @file2 = "testfile.txt", "encrypt.txt"
-  end
+	before :each do
+		@filehandler = EnigmaEncrypto::FileHandler.new
+		@file1, @file2 = "testfile.txt", "encrypt.txt"
+	end
 
-  describe "@filehandler" do
-    it "should be a FileHandler obj" do
-      expect(@filehandler.class).to be EnigmaEncrypto::FileHandler
-    end
-  end
+	describe "@filehandler" do
+		it "should be a FileHandler obj" do
+			expect(@filehandler.class).to be EnigmaEncrypto::FileHandler
+		end
+	end
 
-  describe "#check_file_useability" do
-    it "should return true when the two files are good to go" do
-      expect(@filehandler.check_file_useability(@file1, @file2)) == true
-    end
-    it "should return false if file1 is not eligible for operation" do
-      # testfile2.txt is an empty file.
-      @file1 = "testfile2.txt"
-      expect(@filehandler.check_file_useability(@file1, @file2)) == false
-      # unexistent.txt is an unexistent file.
-      @file1 = "unexistent.txt"
-      expect(@filehandler.check_file_useability(@file1, @file2)) == false
-    end
-    it "should return an operation if file2 exists." do
-      # file to be written to already exists.
-      @file2 = "testfile2.txt"
-      expect(@filehandler.check_file_useability(@file1, @file2).nil?) == false
-    end
-  end
+	describe "#check_file_useability" do
+		it "should return true when the two files are good to go" do
+			expect(@filehandler.check_file_useability(@file1, @file2)) == true
+		end
+		it "should return false if file1 is not eligible for operation" do
+			# testfile2.txt is an empty file.
+			@file1 = "testfile2.txt"
+			expect(@filehandler.check_file_useability(@file1, @file2)) == false
+			# unexistent.txt is an unexistent file.
+			@file1 = "unexistent.txt"
+			expect(@filehandler.check_file_useability(@file1, @file2)) == false
+		end
+		it "should return an operation if file2 exists." do
+			# file to be written to already exists.
+			@file2 = "testfile2.txt"
+			expect(@filehandler.check_file_useability(@file1, @file2).nil?) == false
+		end
+	end
+
+	describe "#file_exist_message" do
+		it "should not return an empty string" do
+			expect(@filehandler.file_exist_message.nil?).to be false
+		end
+	end
+
+	describe "#operation_collector" do
+		it "should collect input from stdin and return corresponding operation" do
+			allow(STDIN).to receive(:gets) { 'C' }
+			expect(@filehandler.send(:operation_collector)).to eq "c"
+		end
+		it "should return false for incorrect entry" do
+			allow(STDIN).to receive(:gets) { 'q' }
+			expect(@filehandler.send(:operation_collector)).to eq false
+		end
+	end
 end
